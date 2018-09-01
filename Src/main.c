df33164bf7859b8dd72825e427a89c1a20eb0e31
Damian Marasco <damian.n.marasco@gmail.com>
2018-09-01 02:01:15 -0300

    Add c var.

--- Src/main.c
+++ Src/main.c
@@ -94,7 +94,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  int c;
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -105,7 +105,7 @@ int main(void)
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
-
+	  c--;
   }
   /* USER CODE END 3 */
 
